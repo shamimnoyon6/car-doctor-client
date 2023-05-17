b19ab52f4470026275bc4b884479d6ceeb24e20f
shamimnoyon6 <shamimnoyon6@gmail.com>
2023-05-17 14:59:52 +0600

    first push

---
+++ src/pages/Home/ServicesCard/ServicesCard.jsx
@@ -0,0 +1,27 @@
+
+
+// eslint-disable-next-line react/prop-types
+const ServicesCard = ({ service }) => {
+
+    // eslint-disable-next-line react/prop-types
+    const { title, img, price } = service;
+
+    return (
+        <div>
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <figure className="px-10 pt-10">
+                    <img src={img} alt="Shoes" className="rounded-xl" />
+                </figure>
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title">{title}</h2>
+                    <p >${price}</p>
+                    <div className="card-actions">
+                        <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ServicesCard;
